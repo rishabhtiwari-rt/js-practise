93fd3a750a6c1a3071d495cdde520a497c1b9fda
rishabhtiwari1357-dot <rishabhtiwari1357@gmail.com>
2026-03-08 05:27:39 +0000

    functions

---
+++ basic3/functions.js
@@ -0,0 +1,16 @@
+// function addTwoNumbers(num1, num2){
+//     console.log(num1 + num2)
+// }
+
+function addTwoNumbers(num1, num2){
+    let result = num1 + num2
+    return result
+}
+
+ addTwoNumbers(4,3)
+// sum = addTwoNumbers(4,3)
+// console.log("result is: ",sum)
+
+// console.log(addTwoNumbers(4, 3))
+// result = addTwoNumbers(2,3)
+// console.log(result)
