5a7bc67be1ee7eebabcc8ee0f9e24b01d3ed86d2
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-03-14 08:44:20 +0000

    exp_truthy

--- control_flow/truthy.js
+++ control_flow/truthy.js
@@ -1,11 +1,26 @@
 const userEmail = []
 
-if(userEmail){
-    console.log("Got user email")
-} else {
-    console.log("don't have user email")
-}
+// if(userEmail){
+//     console.log("Got user email")
+// } else {
+//     console.log("don't have user email")
+// }
 
-if (userEmail.length === 0){
-    console.log("array is empty")
-}
+// if (userEmail.length === 0){
+//     console.log("array is empty")
+// }
+
+const emptyobj = {}
+
+// if (Object.keys(emptyObj).length === 0){
+//     console.log("object is empty");
+// }
+
+
+
+let val1;
+val1 = 5 ?? 10
+val2 = null ?? 10
+val3 = undefined ?? 15
+
+console.log(val3)
