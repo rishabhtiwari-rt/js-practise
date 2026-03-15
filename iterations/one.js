a74ece5f2fb1144b8b55a5b425efa07cb9e67ae6
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-03-15 05:50:48 +0000

    exp_iterations

--- iterations/one.js
+++ iterations/one.js
@@ -4,16 +4,22 @@ for (let i = 0; i <= 10; i++) {
 }
 
 
-for (let i = 0; i <= 10; i++) {
-    console.log(`Outer loop value: ${i}`)
-    for (let j = 0; j <= 10; j++) {
-        // console.log(`Inner loop value ${j} and outer loop ${i}`)
-        console.log(i + '*' + j + '=' + i*j);
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`)
+//     for (let j = 0; j <= 10; j++) {
+//         // console.log(`Inner loop value ${j} and outer loop ${i}`)
+//         console.log(i + '*' + j + '=' + i*j);
         
-    }
+//     }
+    
+// }
+
+
+
+for (let index = 1; index <= 20; index++) {
+    console.log(`value of i is ${index}`)
     
 }
-
 
 
 
