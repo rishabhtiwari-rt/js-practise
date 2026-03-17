bd8498429d030e44eb6e3bc82bbad479ab8c04ac
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-03-17 10:35:01 +0000

    for_each

---
+++ iterations/five.js
@@ -0,0 +1,24 @@
+const coding = ["js", "ruby", "java", "python", "cpp" ]
+
+// coding.forEach(function (value){
+//     console.log(value)
+// })
+
+// coding.forEach( (array_elements) => {
+//     console.log(array_elements)
+// })
+
+// function printMe(array_elements){
+//     console.log(array_elements);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+
+
+
+
