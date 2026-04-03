60a7eaaec843d68d4f2e24870be428781da9efcd
Rishabh Tiwari <rishabhtiwari1357@gmail.com>
2026-04-03 11:44:22 +0530

    objects in js

---
+++ oops/object.js
@@ -0,0 +1,28 @@
+function multiplyby5(num){
+  return num * num;
+}
+
+multiplyby5.power = 2
+
+console.log(multiplyby5(7))
+console.log(multiplyby5.power);
+console.log(multiplyby5.prototype)
+
+
+function createUser(username, score){
+  this.username = username
+  this.score = score
+}
+
+createUser.prototype.increment = function(){
+  this.score++
+}
+
+createUser.prototype.printMe = function(){
+  console.log(`price is ${this.score}`);
+}
+
+const chai = new createUser("chai", 25)
+const tea = createUser("tea", 250)
+
+chai.printMe()
